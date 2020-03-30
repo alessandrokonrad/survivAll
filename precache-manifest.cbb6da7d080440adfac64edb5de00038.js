459fe9f6fc6835e58f6955ea2fbf94edea8fc01c
Alessandro Konrad <akonrad6@gmail.com>
2020-03-30 12:24:04 +0200

    Updates

---
+++ precache-manifest.cbb6da7d080440adfac64edb5de00038.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d760d79850a20b3e0572892ea89ba81c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "423f3aaebe114f401038",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "4af1af55f0be78fc3ab4",
+    "url": "/static/js/2.0f0106df.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.0f0106df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "423f3aaebe114f401038",
+    "url": "/static/js/main.24097b3c.chunk.js"
+  },
+  {
+    "revision": "3f809adccd7b5eb81ce7",
+    "url": "/static/js/runtime-main.6d8ceafa.js"
+  },
+  {
+    "revision": "6b860cb5ae47df6b63834d72cbf75f24",
+    "url": "/static/media/ecology.6b860cb5.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/survivAll/precache-manifest.aa657b4c02fc702b1e17984fcf99ab8b.js"
+  "/precache-manifest.cbb6da7d080440adfac64edb5de00038.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/survivAll/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
