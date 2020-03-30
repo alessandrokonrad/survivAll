be36da8a73ca3555a52c2ea71f596bd52c639065
Alessandro Konrad <akonrad6@gmail.com>
2020-03-30 12:19:58 +0200

    Updates

---
+++ precache-manifest.aa657b4c02fc702b1e17984fcf99ab8b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b21786ab3e4d33d8596f5f412bd12e66",
+    "url": "/survivAll/index.html"
+  },
+  {
+    "revision": "423f3aaebe114f401038",
+    "url": "/survivAll/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "4af1af55f0be78fc3ab4",
+    "url": "/survivAll/static/js/2.0f0106df.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/survivAll/static/js/2.0f0106df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "423f3aaebe114f401038",
+    "url": "/survivAll/static/js/main.24097b3c.chunk.js"
+  },
+  {
+    "revision": "64f6af64f0e78cadb05e",
+    "url": "/survivAll/static/js/runtime-main.b812f01b.js"
+  },
+  {
+    "revision": "6b860cb5ae47df6b63834d72cbf75f24",
+    "url": "/survivAll/static/media/ecology.6b860cb5.svg"
+  }
+]);
